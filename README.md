4beb60bf46376aa71d9ec824bd50d4ce1390e698
kenangghalih21 <kenangghalih21@gmail.com>
2023-04-12 17:32:18 +0700

    finish of flask

--- README.md
+++ README.md
@@ -6,12 +6,13 @@ For the final mission, i choose to scrap the (Hard) one, Data pekerjaan data di 
 - Dari Halaman tersebut carilah title pekerjaan , lokasi pekerjaan , tanggal pekerjaan di post dan dealine submit permohonan, dan perusahaan
 - tariklah 15 halaman
 - Buatlah plot dari jumlah pekerjaan berdasarkan lokasi.
-
 ## Finished skeleton
 - for skeleton, i make new notebook 'Skeleton for Scrapping 15 pages' only to focus scrapping 15 pages from website, the result of unclean data i save into .csv to make it easier if we want to recall it instead of load page from web.
 ![](cat_order.png)
 <b>simple bar plot analysis:</b> jakarta especially jakarta selatan has the most vacancy in 'data' occupation than any location, second is jakarta pusat.
-
+## Result of flask-dashboard
+![](page1.png)
+![](page2.png)
 ## Challenges
 - cleaning data
 ![](df_unclean.png)
--- app.py
+++ app.py
@@ -12,40 +12,129 @@ matplotlib.use('Agg')
 app = Flask(__name__) #do not change this
 
 #insert the scrapping here
-url_get = requests.get('____')
-soup = BeautifulSoup(url_get.content,"html.parser")
+temp = [] # var penampung
+
+for page_num in range(1, 16):    
+    url = f'https://www.kalibrr.id/job-board/te/data/{page_num}'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
 #find your right key here
-____ = soup.find('___')
-___ = ____.find_all('___')
+    Title = soup.find_all('a', attrs={'class':'k-text-primary-color'})[:15]
+    Location = soup.find_all('a', attrs={'class':'k-text-subdued k-block'})[:15]
+    #post_on
+    Post_on = soup.find_all('span', attrs={'class':'k-block k-mb-1'})[:15]
+    #get deadline
+    Deadline = soup.find_all('span', attrs={'class':'k-block k-mb-1'})[:15]
+    # Mencari 15 elemen span dengan class 'class-span' pada halaman tersebut
+    Company = soup.find_all('span', {'class':'k-inline-flex k-items-center k-mb-1'})[:15]
 
-row_length = len(___)
 
-temp = [] #initiating a list 
+    for a in Title:
+        print(a.text.strip())
+        
+    for a in Location:
+        print(a.text.strip())
+        
+    for span in Post_on:
+        print(span.text.strip())
+        
+    for span in Deadline:
+        print(span.text.strip())
 
-for i in range(1, row_length):
+    for span in Company:
+        print(span.text.strip())
+
+
 #insert the scrapping process here
     
-    temp.append((____,____,____)) 
+    temp.append((Title, Location, Post_on, Deadline, Company)) 
 
 temp = temp[::-1]
 
 #change into dataframe
-data = pd.DataFrame(____, columns = ('____','_____','_____'))
+data = []
+for page_data in temp:
+    
+    #1 Mengekstrak list yang telah dibuat dalam looping sebelumnya menjadi informasi text
+    title_new = [Title.text.strip() for Title in page_data[0]]
+    location_new = [Location.text.strip() for Location in page_data[1]]
+    post_on_new = [Post_on.text.strip() for Post_on in page_data[2]]
+    deadline_new = [Deadline.text.strip() for Deadline in page_data[3]]
+    company_new = [Company.text.strip() for Company in page_data[4]]
+    
+    #2 Menambahkan list yang baru saja dibentuk dengan operator penambahan list (+=) dan menggabungkannya dengan list(zip())
+    data += list(zip(title_new, location_new, post_on_new, deadline_new, company_new))
 
+df = pd.DataFrame(data, columns=['title', 'location', 'post_on', 'deadline', 'company'])
+    
 #insert data wrangling here
+df['location'] = df['location'].str.replace(", Indonesia","")
+df['location'] = df['location'].str.replace("Kota Jakarta Barat","Jakarta Barat")
+df['location'] = df['location'].str.replace("Central Jakarta City","Jakarta Pusat")
+df['location'] = df['location'].str.replace("Central Jakarta","Jakarta Pusat")
+df['location'] = df['location'].str.replace("South Jakarta","Jakarta Selatan")
+df['location'] = df['location'].str.replace("North Jakarta","Jakarta Utara")
+df['location'] = df['location'].str.replace("South Tangerang","Tangerang Selatan")
+df['location'] = df['location'].str.replace("West Jakarta","Jakarta Barat")
+df['location'] = df['location'].str.replace("East Jakarta","Jakarta Timur")
+df['location'] = df['location'].str.replace("Kota Jakarta Selatan","Jakarta Selatan")
+df['location'] = df['location'].str.replace("Jakarta Selatan City","Jakarta Selatan")
+df['location'] = df['location'].str.replace("Kota Jakarta Pusat","Jakarta Pusat")
+df['location'] = df['location'].str.replace("Depok City","Depok")
+df['location'] = df['location'].str.replace("Sukabumi City","Sukabumi")
+df['location'] = df['location'].str.replace("West Lombok","Lombok Barat")
 
+# cleaning post_on
+df['post_on'] = df['post_on'].str.split('•').str[0]
+df.post_on = df.post_on.str.replace("Posted","")
+
+# cleaning_deadline
+df.deadline = df.deadline.str[32:]
+
+def clean_data(x):
+    if isinstance(x, str):
+        x = x.replace('e ', '')
+        x = x.replace('re ', '')
+        x = x.replace('r', '')
+        x = x.replace('for ', '')
+        x = x.replace('fo', '')
+        x = x.replace('Ap', 'Apr')
+        x = x.replace('Ma', 'May')
+        x = x.replace('Mayy', 'May')
+    return x
+df['deadline'] = df['deadline'].apply(clean_data)
+# remove space in front
+df.deadline = df.deadline.str.lstrip()
 
 #end of data wranggling 
 
 @app.route("/")
 def index(): 
-	
-	card_data = f'{data["____"].mean().round(2)}' #be careful with the " and ' 
 
-	# generate plot
-	ax = ____.plot(figsize = (20,9)) 
-	
+ 
+	cat_order = df.groupby('location').agg({
+	'title' : 'count'
+	}).sort_values('title', ascending=True).reset_index().tail(15)
+	X = cat_order['location']
+	Y = cat_order['title']
+	my_colors = ['r','g','b','k','y','m','c']
+	fig = plt.figure(figsize=(17,9))
+	fig.add_subplot()
+	plt.barh(X,Y, color=my_colors) 	
+
+	figfile = BytesIO()
+	plt.savefig(figfile, format='png', transparent=True)
+	figfile.seek(0)
+	figdata_png = base64.b64encode(figfile.getvalue())
+	plot_result2 = str(figdata_png)[2:-1]
+
+	card_data = f'{cat_order["title"].mean().round(2)}' #be careful with the " and ' 
+
+
+  	# generate plot
+	plot_result = cat_order.plot(figsize = (17,9), x='location', y='title') 
+ 
 	# Rendering plot
 	# Do not change this
 	figfile = BytesIO()
@@ -53,11 +142,12 @@ def index():
 	figfile.seek(0)
 	figdata_png = base64.b64encode(figfile.getvalue())
 	plot_result = str(figdata_png)[2:-1]
-
+    
 	# render to html
 	return render_template('index.html',
 		card_data = card_data, 
-		plot_result=plot_result
+		plot_result=plot_result,
+		plot_result2=plot_result2
 		)
 
 
